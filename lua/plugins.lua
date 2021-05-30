b36a1d31c668b5debe3c3a949ced052d19e06578
Potato1682 <contact@potato1682.ml>
2021-05-31 02:43:44 +0900

    feat: Project finder

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -34,7 +34,12 @@ return require('packer').startup(function(use)
   use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
 
   use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } } }
-  use 'nvim-telescope/telescope-media-files.nvim'
+  use {
+    'nvim-telescope/telescope-media-files.nvim',
+    'nvim-telescope/telescope-fzy-native.nvim',
+    'nvim-telescope/telescope-project.nvim',
+    requires = { 'nvim-telescope/telescope.nvim' }
+  }
 
   use 'glepnir/dashboard-nvim'
 
