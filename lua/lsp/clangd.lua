9fa1326c35cd1a6a37182bf32375fb4fefab4246
Potato1682 <contact@potato1682.ml>
2021-05-31 12:36:03 +0900

    fix: LSP-server file path

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -1,4 +1,4 @@
-local bin = vim.fn.stdpath("data") .. "/lspinstall/cpp/clangd"
+local bin = vim.fn.stdpath("data") .. "/lspinstall/cpp/clangd/bin/clangd"
 
 if vim.fn.filereadable(bin) == 0 then require("lspinstall").install_server("cpp") end
 
