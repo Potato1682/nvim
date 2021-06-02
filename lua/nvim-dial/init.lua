31e88bd67165024f3bd583c6f2ad0ff07df207ee
Potato1682 <contact@potato1682.ml>
2021-06-03 06:43:37 +0900

    feat: dial

---
+++ lua/nvim-dial/init.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_set_keymap("n", "<C-a>", "<Plug>dial-increment", {})
+vim.api.nvim_set_keymap("n", "<C-x>", "<Plug>dial-decrement", {})
+vim.api.nvim_set_keymap("v", "<C-a>", "<Plug>dial-increment", {})
+vim.api.nvim_set_keymap("v", "<C-x>", "<Plug>dial-decrement", {})
+vim.api.nvim_set_keymap("v", "g<C-a>", "<Plug>dial-increment-additional", {})
+vim.api.nvim_set_keymap("v", "g<C-x>", "<Plug>dial-decrement-additional", {})
+
