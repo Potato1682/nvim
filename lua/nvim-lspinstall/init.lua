f1fa0d5d9629e475dfe21a9a4d529fcc696df4c6
Potato1682 <contact@potato1682.ml>
2021-06-03 06:54:18 +0900

    perf: remove unused installers

--- lua/nvim-lspinstall/init.lua
+++ lua/nvim-lspinstall/init.lua
@@ -18,35 +18,6 @@ require'lspinstall/servers'.eslintd = vim.tbl_extend("error", {}, {
   ]]
 })
 
--- Use jq to use collect download url
-
-local lua_config = require("lspinstall/util").extract_config("sumneko_lua")
-
-require("lspinstall/servers").lua = vim.tbl_extend("error", lua_config, {
-  install_script = [[
-    os=$(uname -s | tr "[:upper:]" "[:lower:]")
-    case $os in
-      linux)
-        platform="Linux"
-      ;;
-      darwin)
-        platform="macOS"
-      ;;
-    esac
-
-    curl -Lo sumneko-lua.vsix $(curl -s "https://api.github.com/repos/sumneko/vscode-lua/releases/latest" | jq . | grep -E "https?://.*\.vsix" | cut -d : -f 2,3 | tr -d \")
-    unzip sumneko-lua.vsix -d sumneko-lua
-    rm sumneko-lua.vsix
-
-    chmod +x sumneko-lua/extension/server/bin/$platform/lua-language-server
-
-    echo "#!/usr/bin/env bash" > sumneko-lua-language-server
-    echo "\$(dirname \$0)/sumneko-lua/server/bin/$platform/lua-language-server -E -e LANG=en \$(dirname \$0)/sumneko-lua/extension/server/main.lua \$*" >> sumneko-lua-language-server
-
-    chmod +x sumneko-lua-language-server
-  ]]
-})
-
 local clangd_config = require("lspinstall/util").extract_config("clangd")
 
 require("lspinstall/servers").cpp = vim.tbl_extend('error', clangd_config, {
@@ -66,28 +37,6 @@ require("lspinstall/servers").cpp = vim.tbl_extend('error', clangd_config, {
     unzip clangd.zip
     rm clangd.zip
     mv clangd_* clangd
-  ]]
-})
-
-local omnisharp_config = require("lspinstall/util").extract_config("omnisharp")
-
-require("lspinstall/servers").csharp = vim.tbl_extend("error", omnisharp_config, {
-  install_script = [[
-    os=$(uname -s | tr "[:upper:]" "[:lower:]")
-
-    case $os in
-      linux)
-        platform="linux-x64"
-        ;;
-      darwin)
-        platform="osx"
-        ;;
-    esac
-    
-    curl -Lo omnisharp.zip $(curl -s https://api.github.com/repos/OmniSharp/omnisharp-roslyn/releases/latest | jq . | grep -E "browser_.+" | cut -d'"' -f4 | grep "omnisharp-$platform.zip")
-    unzip omnisharp.zip -d omnisharp
-    rm omnisharp.zip
-    chmod +x omnisharp/run
   ]]
 })
 
