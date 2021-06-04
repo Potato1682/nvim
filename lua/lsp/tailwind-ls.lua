2200ee020e80525c3d9cbd2dc47ae90966fbcceb
Potato1682 <contact@potato1682.ml>
2021-06-04 09:46:47 +0900

    fix: tailwind init error

--- lua/lsp/tailwind-ls.lua
+++ lua/lsp/tailwind-ls.lua
@@ -1,11 +1,15 @@
+local util = require("lspconfig/util")
+
 local bin = vim.fn.stdpath("data") .. "/lspinstall/tailwindcss/tailwindcss-intellisense/extension/dist/server/tailwindServer.js"
 
 if vim.fn.filereadable(bin) == 0 then require("lspinstall").install_server("tailwindcss") end
 
 require"lspconfig.configs".tailwindls = {
-  default_config = { cmd = { "node", bin }, filetypes = { "html", "typescriptreact", "vue" } },
-  docs = { description = [[ ]], default_config = {} }
+  default_config = {
+    cmd = { "node", bin },
+    filetypes = { "html", "svelte", "typescriptreact", "vue" },
+    root_dir = util.root_pattern("package.json", ".git")
+  },
+  docs = { description = [[ ]], default_config = { root_dir = [[ root_pattern("package.json", ".git") ]] } }
 }
-
-require"lspconfig".tailwindls.setup()
 
