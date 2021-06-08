2af11a99e529df2bccd9ae9a68c71e5c04266b4e
Potato1682 <contact@potato1682.ml>
2021-06-09 00:23:17 +0900

    feat: rainbow indent line on: live-media

--- lua/nvim-indentline/init.lua
+++ lua/nvim-indentline/init.lua
@@ -9,4 +9,21 @@ vim.g.indent_blankline_context_patterns = {
     'arguments', 'if_statement', 'else_clause', 'jsx_element', 'jsx_self_closing_element', 'try_statement',
     'catch_clause', 'import_statement', 'operation_type'
 }
+vim.g.indent_blankline_space_char = "·"
+
+vim.cmd [[ hi IndentRainbow1 guifg=#be5046 guibg=NONE gui=nocombine ]]
+vim.cmd [[ hi IndentRainbow2 guifg=#e5c07b guibg=NONE gui=nocombine ]]
+vim.cmd [[ hi IndentRainbow3 guifg=#98c379 guibg=NONE gui=nocombine ]]
+vim.cmd [[ hi IndentRainbow4 guifg=#56b6c2 guibg=NONE gui=nocombine ]]
+vim.cmd [[ hi IndentRainbow5 guifg=#61afef guibg=NONE gui=nocombine ]]
+vim.cmd [[ hi IndentRainbow6 guifg=#c678dd guibg=NONE gui=nocombine ]]
+
+vim.g.indent_blankline_char_highlight_list = {
+  "IndentRainbow1",
+  "IndentRainbow2",
+  "IndentRainbow3",
+  "IndentRainbow4",
+  "IndentRainbow5",
+  "IndentRainbow6"
+}
 
