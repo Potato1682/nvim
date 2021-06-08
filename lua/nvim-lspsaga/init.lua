8016243df558dbe188200a99b307c78829134bf4
Potato1682 <contact@potato1682.ml>
2021-06-09 00:22:27 +0900

    fix: lightbulb shows only on virtual text on: live-media

--- lua/nvim-lspsaga/init.lua
+++ lua/nvim-lspsaga/init.lua
@@ -1,4 +1,8 @@
-require("lspsaga").init_lsp_saga { use_saga_diagnostic_sign = true, error_sign = "", warn_sign = "", infor_sign = "", rename_prompt_prefix = ">" }
+require("lspsaga").init_lsp_saga { use_saga_diagnostic_sign = true, error_sign = "", warn_sign = "", infor_sign = "", rename_prompt_prefix = ">", code_action_prompt = {
+  enable = true,
+  sign = false,
+  virtual_text = true
+} }
 
 vim.cmd([[ autocmd CursorHold,CursorHoldI * Lspsaga hover_doc ]])
 
