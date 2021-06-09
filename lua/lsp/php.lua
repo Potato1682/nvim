81947ddc797fecb0dcb79e6b06f2aafb27d41ca4
Potato1682 <contact@potato1682.ml>
2021-06-09 22:27:49 +0900

    fix: intelephense path on: live-media

--- lua/lsp/php.lua
+++ lua/lsp/php.lua
@@ -1,4 +1,4 @@
-local bin = vim.fn.stdpath("data") .. "/lspinstall/php/intelephense"
+local bin = vim.fn.stdpath("data") .. "/lspinstall/php/node_modules/.bin/intelephense"
 
 if vim.fn.filereadable(bin) == 0 then require("lspinstall").install_server("php") end
 
