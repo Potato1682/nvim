6e3a91aae3daf92963d2755667c335d9ac2387b5
Potato1682 <contact@potato1682.ml>
2021-06-14 04:12:49 +0900

    fix: add parentheses after <Plug>

--- lua/nvim-dial/init.lua
+++ lua/nvim-dial/init.lua
@@ -1,7 +1,14 @@
-vim.api.nvim_set_keymap("n", "<C-a>", "<Plug>dial-increment", {})
-vim.api.nvim_set_keymap("n", "<C-x>", "<Plug>dial-decrement", {})
-vim.api.nvim_set_keymap("v", "<C-a>", "<Plug>dial-increment", {})
-vim.api.nvim_set_keymap("v", "<C-x>", "<Plug>dial-decrement", {})
-vim.api.nvim_set_keymap("v", "g<C-a>", "<Plug>dial-increment-additional", {})
-vim.api.nvim_set_keymap("v", "g<C-x>", "<Plug>dial-decrement-additional", {})
+vim.api.nvim_set_keymap("n", "<C-a>", "<Plug>(dial-increment)", {})
+vim.api.nvim_set_keymap("n", "<C-x>", "<Plug>(dial-decrement)", {})
+vim.api.nvim_set_keymap("v", "<C-a>", "<Plug>(dial-increment)", {})
+vim.api.nvim_set_keymap("v", "<C-x>", "<Plug>(dial-decrement)", {})
+vim.api.nvim_set_keymap("v", "g<C-a>", "<Plug>(dial-increment-additional)", {})
+vim.api.nvim_set_keymap("v", "g<C-x>", "<Plug>(dial-decrement-additional)", {})
+
+local dial = require("dial")
+
+dial.augends["custom#boolean"] = dial.common.enum_cyclic{
+  name = "boolean",
+  strlist = { "true", "false" },
+}
 
