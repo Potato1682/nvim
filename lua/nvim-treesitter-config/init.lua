95603b5df746569609a4e69235705394ce00278b
Potato1682 <contact@potato1682.ml>
2021-06-14 07:20:15 +0900

    fix: add missing plugin settings

--- lua/nvim-treesitter-config/init.lua
+++ lua/nvim-treesitter-config/init.lua
@@ -5,6 +5,20 @@ require'nvim-treesitter.configs'.setup {
   indent = { enable = { "javascriptreact", "typescriptreact" } },
   autotag = { enable = true },
   rainbow = { enable = true },
-  context_commentstring = { enable = true, enable_autocmd = true }
+  context_commentstring = { enable = true, enable_autocmd = true },
+  node_movement = {
+    keymaps = {
+      move_up = "<a-k>",
+      move_down = "<a-j>",
+      move_left = "<a-h>",
+      move_right = "<a-l>",
+      swap_left = "<s-a-h>",
+      swap_right = "<s-a-l>",
+      select_current_node = "<leader><Cr>"
+    },
+    swappable_textobjects = { "@function.outer", "@parameter.inner", "@statement.outer" },
+    allow_switch_parents = true,
+    allow_next_parent = true
+  }
 }
 
