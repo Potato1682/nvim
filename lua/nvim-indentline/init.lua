58c36cfe79c98095f9881fb58b8fef7a07ca4bb7
Potato1682 <contact@potato1682.ml>
2021-06-14 04:19:09 +0900

    fix: do not show indentline on lspsaga hover window

--- lua/nvim-indentline/init.lua
+++ lua/nvim-indentline/init.lua
@@ -1,5 +1,5 @@
 vim.g.indent_blankline_buftype_exclude = { 'terminal' }
-vim.g.indent_blankline_filetype_exclude = { 'man', 'help', 'nvimtree', 'dashboard', 'packer', 'neogitstatus', 'markdown', 'gesture' }
+vim.g.indent_blankline_filetype_exclude = { 'man', 'help', 'nvimtree', 'dashboard', 'packer', 'neogitstatus', 'markdown', 'gesture', 'sagahover' }
 vim.g.indent_blankline_char = '▏'
 vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_trailing_blankline_indent = false
