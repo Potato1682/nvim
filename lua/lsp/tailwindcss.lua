b5a37b7bd7d2bc77d9291d28b9b6e68d107a32e6
Potato1682 <contact@potato1682.ml>
2021-06-15 04:52:38 +0900

    fix: use lspconfig built-in configuration instead of config registering

--- lua/lsp/tailwindcss.lua
+++ lua/lsp/tailwindcss.lua
@@ -1,15 +1,8 @@
-local util = require("lspconfig/util")
-
 local bin = vim.fn.stdpath("data") .. "/lspinstall/tailwindcss/tailwindcss-intellisense/extension/dist/server/tailwindServer.js"
 
 if vim.fn.filereadable(bin) == 0 then require("lspinstall").install_server("tailwindcss") end
 
-require"lspconfig.configs".tailwindls = {
-  default_config = {
-    cmd = { "node", bin },
-    filetypes = { "html", "svelte", "typescriptreact", "vue" },
-    root_dir = util.root_pattern("package.json", ".git")
-  },
-  docs = { description = [[ ]], default_config = { root_dir = [[ root_pattern("package.json", ".git") ]] } }
+require"lspconfig".tailwindcss.setup {
+  cmd = { "node", bin, "--stdio" }
 }
 
