2faadd632dd8bf98e2c124847ad2a0721b063baa
Potato1682 <contact@potato1682.ml>
2021-06-17 20:06:20 +0900

    perf: use default config to use emmet lsp

--- lua/lsp/emmet.lua
+++ lua/lsp/emmet.lua
@@ -6,13 +6,21 @@ local lspconfig = require("lspconfig")
 local configs = require("lspconfig/configs")
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.window = capabilities.window or {}
+capabilities.window.workDoneProgress = true
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 if not lspconfig.emmet_ls then
   configs.emmet_ls = {
-    default_config = { cmd = { bin, "--stdio" }, filetypes = { "html", "css", "scss", "vue" }, root_dir = vim.loop.cwd, settings = {} }
+    default_config = {
+      cmd = { bin, "--stdio" },
+      filetypes = { "html", "css", "scss", "vue" },
+      root_dir = vim.loop.cwd,
+      settings = {}
+    }
   }
 end
 
-lspconfig.emmet_ls.setup { capabilities = capabilities };
+lspconfig.emmet_ls.setup { capabilities = capabilities }
 
