b65cb0fcf527e49b7077da90aa0d54d646f3adb5
Potato1682 <contact@potato1682.ml>
2021-06-22 16:49:05 +0900

    fix: use *.json instead of * in hexokinase glob

--- lua/nvim-hexokinase/init.lua
+++ lua/nvim-hexokinase/init.lua
@@ -1,3 +1,3 @@
 vim.g.Hexokinase_highlighters = { "sign_column" }
-vim.g.Hexokinase_palettes = vim.fn.split(vim.fn.glob(vim.fn.stdpath("config") .. "/colors/*"), "\n")
+vim.g.Hexokinase_palettes = vim.fn.split(vim.fn.glob(vim.fn.stdpath("config") .. "/colors/*.json"), "\n")
 
