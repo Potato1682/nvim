234fc6bb48d990ac9fbf823bb604d6e99b42edfe
Potato1682 <contact@potato1682.ml>
2021-06-22 16:37:36 +0900

    fix: virtual-text toggle now works properly

--- lua/lsp/virtual-text.lua
+++ lua/lsp/virtual-text.lua
@@ -2,14 +2,28 @@ local virtual_text = {}
 
 virtual_text.show = true
 
-virtual_text.toggle = function()
-  virtual_text.show = not virtual_text.show
-  vim.lsp.diagnostic.display(
-    vim.lsp.diagnostic.get(0, 1),
-    0,
-    1,
-    { virtual_text = virtual_text.show }
-  )
+function virtual_text.toggle()
+  if virtual_text.show then
+    virtual_text.show = false
+
+    vim.lsp.diagnostic.clear(0)
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+
+    print("Diagnostics are hidden")
+  else
+    virtual_text.show = true
+
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+      vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+      }
+    )
+
+    print("Diagnostics are visible")
+  end
 end
 
 return virtual_text
