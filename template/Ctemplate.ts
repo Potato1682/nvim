b7661462208285396ee28ef801bdb8e7fbe26b5f
Potato1682 <contact@potato1682.ml>
2021-06-30 12:22:28 +0900

    fix: change renaming file extension from .java to .ts

--- template/Ctemplate.ts
+++ template/Ctemplate.ts
@@ -1,5 +1,5 @@
 :%s;<+CLASS NAME+>;\=substitute(expand('%:t:r'), '\CC\ze\u\w*', '', '')
-:call rename(expand('%'), expand('%:p:h') . "/" . substitute(expand('%'), '\CC\ze\u\w*\.ts', '', '')) | lua vim.cmd("f " .. vim.fn.expand('%:p:h') .. "/" .. vim.fn.substitute(vim.fn.expand('%:t:r'), [[\CC\ze\u\w*]], '', '') .. ".java") 
+:call rename(expand('%'), expand('%:p:h') . "/" . substitute(expand('%'), '\CC\ze\u\w*\.ts', '', '')) | lua vim.cmd("f " .. vim.fn.expand('%:p:h') .. "/" .. vim.fn.substitute(vim.fn.expand('%:t:r'), [[\CC\ze\u\w*]], '', '') .. ".ts") 
 package <+PACKAGE+>
 
 class <+CLASS NAME+> {
