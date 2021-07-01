c5fbe35de97329afb17fc6c262a30b1649075a09
Potato1682 <contact@potato1682.ml>
2021-07-01 20:36:06 +0900

    fix: docker language server reference

--- lua/lsp/docker.lua
+++ lua/lsp/docker.lua
@@ -1,16 +1,26 @@
-local container = require "lspcontainers"
 local lsp_config = require "lsp"
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 capabilities.window = capabilities.window or {}
 capabilities.window.workDoneProgress = true
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 require("lspconfig").dockerls.setup {
   before_init = function(params)
     params.processId = vim.NIL
   end,
-  cmd = container.command "dockerls",
+  cmd = {
+    "docker",
+    "container",
+    "run",
+    "--interactive",
+    "--rm",
+    "--volume",
+    vim.loop.cwd() .. ":" .. vim.loop.cwd(),
+    "rcjsuen/docker-langserver",
+    "--stdio",
+  },
   root_dir = vim.loop.cwd,
   capabilities = capabilities,
   on_attach = lsp_config.common_on_attach,
