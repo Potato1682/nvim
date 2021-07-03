8178f0ecae514f3dcff13574df5d208eac76a668
Potato1682 <contact@potato1682.ml>
2021-07-03 20:56:09 +0900

    feat: <%= %> to evaluate in template files

--- lua/nvim-template/init.lua
+++ lua/nvim-template/init.lua
@@ -1,3 +1,11 @@
+-- Define filename replace keyword
 vim.cmd [[ autocmd User plugin-template-loaded silent! %s/<+FILE NAME+>/\=expand('%:t')/g ]]
+
+-- Define ClassName replace keyword
 vim.cmd [[ autocmd User plugin-template-loaded silent! %s/<+CLASS NAME+>/\=expand('%:t:r')/g ]]
+
+-- Evaluate in vim
+vim.cmd [[ autocmd User plugin-template-loaded silent %s/<%=\(.\{-}\)%>/\=eval(submatch(1))/ge ]]
+
+-- Move the cursor to this
 vim.cmd [[ autocmd User plugin-template-loaded if search('<+CURSOR+>') | execute 'normal! "_da>' | endif ]]
