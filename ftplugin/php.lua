6f2b4562b3758c3db66d1fffb032de3b84913e7a
Potato1682 <contact@potato1682.ml>
2021-07-04 22:30:06 +0900

    perf: load intelephense LSP when detect PHP

--- ftplugin/php.lua
+++ ftplugin/php.lua
@@ -1,3 +1,21 @@
+local lsp_config = require "lsp"
+local bin = vim.fn.stdpath "data" .. "/lspinstall/php/node_modules/.bin/intelephense"
+
+if vim.fn.filereadable(bin) == 0 then
+  require("lspinstall").install_server "php"
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.window = capabilities.window or {}
+capabilities.window.workDoneProgress = true
+
+require("lspconfig").intelephense.setup {
+  cmd = { bin, "--stdio" },
+  on_attach = lsp_config.common_on_attach,
+  capabilities = capabilities,
+}
+
 local debug_install_dir = vim.fn.stdpath "data" .. "/dapinstall/php/php-debug/"
 
 if vim.fn.glob(debug_install_dir) == "" then
