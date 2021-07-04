6e7b72bc94cc4ee190d36402a69914803a8d97dc
Potato1682 <contact@potato1682.ml>
2021-07-04 20:31:04 +0900

    perf: load ruby LSP and DAP when detect ruby

--- ftplugin/ruby.lua
+++ ftplugin/ruby.lua
@@ -1,3 +1,21 @@
+local lsp_config = require "lsp"
+local bin = vim.fn.stdpath "data" .. "/lspinstall/ruby/solargraph/bin/solargraph"
+
+if vim.fn.filereadable(bin) == 0 then
+  require("lspinstall").install_server "ruby"
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.window = capabilities.window or {}
+capabilities.window.workDoneProgress = true
+
+require("lspconfig").solargraph.setup {
+  cmd = { bin, "stdio" },
+  on_attach = lsp_config.common_on_attach,
+  capabilities = capabilities,
+}
+
 local debug_install_dir = vim.fn.stdpath "data" .. "/dapinstall/ruby/"
 
 if vim.fn.glob(debug_install_dir) == "" then
