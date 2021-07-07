35dabb0bbe4ce60a4e819ec931cddee8ac38070c
Potato1682 <contact@potato1682.ml>
2021-07-07 22:14:03 +0900

    fix: remove gitsigns default config

--- lua/nvim-gitsigns/init.lua
+++ lua/nvim-gitsigns/init.lua
@@ -6,15 +6,7 @@ require("gitsigns").setup {
     topdelete = { hl = "GitSignsDelete", text = "", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
     changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
   },
-  numhl = false,
-  linehl = false,
   keymaps = { noremap = true, buffer = true },
-  watch_index = { interval = 1000 },
-  sign_priority = 6,
-  update_debounce = 200,
-  status_formatter = nil,
-  use_decoration_api = true,
-  use_internal_diff = true,
   current_line_blame = true,
 }
 
