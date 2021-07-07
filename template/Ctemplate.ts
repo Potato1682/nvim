36cee1794b3bf8bd7c2e553388c2c52f7425f7aa
Potato1682 <contact@potato1682.ml>
2021-07-07 20:01:06 +0900

    fix: allow lowercase typescript filename template creating

--- template/Ctemplate.ts
+++ template/Ctemplate.ts
@@ -1,9 +1,9 @@
 :%s;<+CLASS NAME+>;\=substitute(expand('%:t:r'), '\CC\ze\u\w*', '', '')
-:call rename(expand('%'), expand('%:p:h') . "/" . substitute(expand('%'), '\CC\ze\u\w*\.ts', '', '')) | lua vim.cmd("f " .. vim.fn.expand('%:p:h') .. "/" .. vim.fn.substitute(vim.fn.expand('%:t:r'), [[\CC\ze\u\w*]], '', '') .. ".ts") 
+:call rename(expand('%'), expand('%:p:h') . "/" . substitute(expand('%'), '\CC\ze\w*\.ts', '', '')) | lua vim.cmd("f " .. vim.fn.expand('%:p:h') .. "/" .. vim.fn.substitute(vim.fn.expand('%:t:r'), [[\CC\ze\w*]], '', '') .. ".ts")
 package <+PACKAGE+>
 
 class <+CLASS NAME+> {
-	<+CURSOR+>
+  <+CURSOR+>
 }
 
 export default <+CLASS NAME+>
