aa8c6c53e76a1778b47af54b9afe83c830914f6b
Potato1682 <contact@potato1682.ml>
2021-07-09 03:00:04 +0900

    fix: use blue cursor in GUI

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -6,5 +6,6 @@ vim.g.edge_diagnostic_line_highlight = 1
 vim.g.edge_show_eob = 0
 vim.g.edge_sign_column_background = "none"
 vim.g.edge_better_performance = O.edge_better_performance and 1 or 0
+vim.g.edge_cursor = "blue"
 
 vim.cmd("colorscheme " .. O.colorscheme)
