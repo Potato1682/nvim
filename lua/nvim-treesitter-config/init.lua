3036b48d64430f44ebf8d1ac9f9d314d70276835
Potato1682 <contact@potato1682.ml>
2021-07-09 02:37:53 +0900

    fix: remove unused plugins

--- lua/nvim-treesitter-config/init.lua
+++ lua/nvim-treesitter-config/init.lua
@@ -18,20 +18,6 @@ require("nvim-treesitter.configs").setup {
     disable = { "vue", "html" },
   },
   context_commentstring = { enable = true, enable_autocmd = true },
-  node_movement = {
-    keymaps = {
-      move_up = "<a-k>",
-      move_down = "<a-j>",
-      move_left = "<a-h>",
-      move_right = "<a-l>",
-      swap_left = "<s-a-h>",
-      swap_right = "<s-a-l>",
-      select_current_node = "<leader><Cr>",
-    },
-    swappable_textobjects = { "@function.outer", "@parameter.inner", "@statement.outer" },
-    allow_switch_parents = true,
-    allow_next_parent = true,
-  },
   textsubjects = {
     enable = true,
     keymaps = {
