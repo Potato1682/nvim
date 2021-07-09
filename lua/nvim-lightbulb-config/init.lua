1ce44e44ac417903f366e67616c99e73e881bc8f
Potato1682 <contact@potato1682.ml>
2021-07-10 08:17:07 +0900

    fix: emit lightbulb on CursorMoved

--- lua/nvim-lightbulb-config/init.lua
+++ lua/nvim-lightbulb-config/init.lua
@@ -1,2 +1,2 @@
 vim.cmd [[ hi link LightBulbVirtualText Green ]]
-vim.cmd [[ autocmd CursorHold,CursorHoldI * lua require"nvim-lightbulb".update_lightbulb({ sign = { enabled = false }, virtual_text = { enabled = true, text = " " }, status_text = { enabled = true, text = " " }}) ]]
+vim.cmd [[ autocmd CursorMoved,CursorMovedI * lua require"nvim-lightbulb".update_lightbulb({ sign = { enabled = false }, virtual_text = { enabled = true, text = " " }, status_text = { enabled = true, text = " " }}) ]]
