e1916a77bf7d814b34a36fd32da01a0cf4b57e3d
Potato1682 <contact@potato1682.ml>
2021-07-10 00:02:43 +0900

    fix: fix process timeout

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,8 +3,9 @@ local set = vim.api.nvim_set_var
 set("dein#auto_recache", 1)
 
 set("dein#lazy_rplugins", 1)
-set("dein#enable_notification", 1)
-set("dein#install_max_processes", 64)
+set("dein#types#git#clone_depth", 1)
+set("dein#install_process_timeout", 300)
+set("dein#install_progress_type", "tabline")
 set("dein#install_message_type", "none")
 set("dein#enable_name_conversion", 1)
 
@@ -17,7 +18,7 @@ local dein_repository = dein_directory .. "/repos/github.com/Shougo/dein.vim"
 
 vim.opt.runtimepath = dein_repository .. "," .. vim.opt.runtimepath._value
 
-if vim.call("dein#load_state", dein_directory) == 1 then
+if vim.fn["dein#load_state"](dein_directory) == 1 then
   local dein_toml_directory = vim.fn.stdpath "config" .. "/dein"
   local dein_toml = dein_toml_directory .. "/base.toml"
   local dein_toml_lazy = dein_toml_directory .. "/lazy.toml"
@@ -31,7 +32,7 @@ if vim.call("dein#load_state", dein_directory) == 1 then
   vim.fn["dein#save_state"]()
 end
 
-if vim.fn["dein#check_install"] ~= 0 then
+if vim.fn.has("vim_starting") ~= 0 and vim.fn["dein#check_install"] ~= 0 then
   vim.fn["dein#install"]()
   vim.fn["dein#remote_plugins"]()
 end
@@ -40,4 +41,5 @@ local unused_plugins = vim.fn["dein#check_clean"]()
 
 if vim.fn.len(unused_plugins) > 0 then
   vim.fn.map(unused_plugins, "delete(v:val, 'rf')")
+  vim.fn["dein#recache_runtimepath"]()
 end
