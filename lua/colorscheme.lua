e4cb8511b8905961a5ccdaa5332020f888fcaef6
Potato1682 <contact@potato1682.ml>
2021-07-13 22:14:07 +0900

    fix: remove edge sign configuration

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -4,7 +4,6 @@ vim.g.edge_diagnostic_text_highlight = 0
 vim.g.edge_diagnostic_virtual_text = "colored"
 vim.g.edge_diagnostic_line_highlight = 1
 vim.g.edge_show_eob = 0
-vim.g.edge_sign_column_background = "none"
 vim.g.edge_better_performance = O.edge_better_performance and 1 or 0
 vim.g.edge_cursor = "blue"
 
