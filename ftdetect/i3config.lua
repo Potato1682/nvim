17f62d59a6adff9f91cfb81c7fcbbb48ef59fbd6
Potato1682 <contact@potato1682.ml>
2021-08-16 19:24:56 +0900

    feat: i3 and sway support

---
+++ ftdetect/i3config.lua
@@ -0,0 +1,15 @@
+local commands = {
+  i3configdetect = {
+    { "BufNewFile,BufRead", ".i3.config", "setfiletype i3config" },
+    { "BufNewFile,BufRead", "i3.config", "setfiletype i3config" },
+    { "BufNewFile,BufRead", "*.i3.config", "setfiletype i3config" },
+    { "BufNewFile,BufRead", "*.i3config", "setfiletype i3config" },
+    { "BufNewFile,BufRead", "*i3/config", "setfiletype i3config" },
+    { "BufNewFile,BufRead", "*sway/config", "setfiletype i3config" },
+    { "BufNewFile,BufRead", "*.swayconfig", "setfiletype i3config" },
+    { "BufNewFile,BufRead", "*.sway.config", "setfiletype i3config" },
+    { "BufNewFile,BufRead", "sway.config", "setfiletype i3config" },
+  }
+}
+
+require("events").nvim_create_augroups(commands)
