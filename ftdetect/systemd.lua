955f8654f13767eb21799ebf790833b7767c1934
Potato1682 <contact@potato1682.ml>
2021-08-16 19:20:10 +0900

    feat: systemd support

---
+++ ftdetect/systemd.lua
@@ -0,0 +1,20 @@
+local commands = {
+  systemddetect = {
+    { "BufNewFile,BufRead", "*.automount", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.mount", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.path", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.service", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.socket", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.swap", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.target", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.timer", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.slice", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.nspwan", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.unit", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.link", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.netdev", "setfiletype systemd" },
+    { "BufNewFile,BufRead", "*.network", "setfiletype systemd" },
+  },
+}
+
+require("events").nvim_create_augroups(commands)
