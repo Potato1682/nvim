3d48ac1ea0d8ba6b7f921588e6106de04e6a4827
Potato1682 <contact@potato1682.ml>
2021-08-16 19:31:29 +0900

    feat: python requirements.txt support

---
+++ ftdetect/requirements.lua
@@ -0,0 +1,13 @@
+local commands = {
+  requirementsdetect = {
+    { "BufNewFile,BufRead", "requirements.txt", "setfiletype requirements" },
+    { "BufNewFile,BufRead", "constraints.txt", "setfiletype requirements" },
+    { "BufNewFile,BufRead", "dev-requirements.txt", "setfiletype requirements" },
+    { "BufNewFile,BufRead", "requirements/dev.txt", "setfiletype requirements" },
+    { "BufNewFile,BufRead", "requires/tests.txt", "setfiletype requirements" },
+    { "BufNewFile,BufRead", "requirements.in", "setfiletype requirements" },
+    { "BufNewFile,BufRead", "*.pip", "setfiletype requirements" },
+  }
+}
+
+require("events").nvim_create_augroups(commands)
