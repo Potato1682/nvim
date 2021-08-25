88c304a7565b70ee6d226d23c43af3e7bed1c1d2
Potato1682 <contact@potato1682.ml>
2021-08-26 04:29:42 +0900

    fix: add some test runner argument for ultest

--- lua/nvim-ultest/init.lua
+++ lua/nvim-ultest/init.lua
@@ -9,3 +9,7 @@ local icons = require "nvim-nonicons"
 vim.g.ultest_pass_sign = icons.get "check-circle"
 vim.g.ultest_running_sign = icons.get "sync"
 vim.g.ultest_fail_sign = icons.get "x-circle"
+
+vim.g["test#python#pytest#options"] = "--color=yes"
+vim.g["test#javascript#jest#options"] = "--color=always"
+vim.g["test#javascript#reactscripts#options"] = "--watchAll=false"
