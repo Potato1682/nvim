5f94b44f92d3b2bee62296a9e9656c123318dbc9
Potato1682 <contact@potato1682.ml>
2021-08-27 03:33:15 +0900

    chore: add put function for inspect

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+function _G.put(...)
+  local objects = {}
+
+  for i = 1, select("#", ...) do
+    local v = select(i, ...)
+
+    table.insert(objects, vim.inspect(v))
+  end
+
+  print(table.concat(objects, "\n --\n"))
+
+  return ...
+end
+
 require "nvim-globals"
 require "settings"
 require "events"
