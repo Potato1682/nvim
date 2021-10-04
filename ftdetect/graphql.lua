5940732baba4779c96b1171643a9fc15068dcae4
Potato1682 <contact@potato1682.ml>
2021-10-04 21:00:34 +0900

    fix: remove unused ftdetects

--- ftdetect/graphql.lua
+++
@@ -1,9 +0,0 @@
-local commands = {
-  gqldetect = {
-    { "BufNewFile,BufRead", "*.graphql", "setfiletype graphql" },
-    { "BufNewFile,BufRead", "*.graphqls", "setfiletype graphql" },
-    { "BufNewFile,BufRead", "*.gql", "setfiletype graphql" },
-  },
-}
-
-require("events").nvim_create_augroups(commands)
